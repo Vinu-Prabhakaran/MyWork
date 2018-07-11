396b753eb99c4193805280331c9a6c8a871935aa
Vinu Prabhakaran <vinu.cp@gmail.com>
2018-07-10 23:20:45 -0400

    Revert "Started with SpringBoot. Course Api initial set up - Done on Photon. Im going back 2 Oxygen"
    This reverts commit 432b0037321b4acf0a6d2361b2f96f18e6794f6f.

--- LearningJavaScript/src/mainES6.js
+++ LearningJavaScript/src/mainES6.js
@@ -1,9 +1,2 @@
 //document.querySelector("#container").innerHTML="Helloons";
 document.getElementById('container').innerHTML = "How are you Vinu";
-
-let x = 10;
-if (x>9){
-     x=11;
-    console.log('x in if block '+x);
-}
-console.log('x outside if block '+x);
