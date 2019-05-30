944603b90d62b1e00ccbecdacec2a3e7cfd8e092
Vinu Prabhakaran <vinu.cp@gmail.com>
2019-05-30 00:05:10 -0400

    Spring Framework - Changes related reading message source from property file.

--- SpringLegacy/src/main/java/com/vinu/spring/Circle.java
+++ SpringLegacy/src/main/java/com/vinu/spring/Circle.java
@@ -3,6 +3,9 @@
  */
 package com.vinu.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+
 /**
  * @author Vinu Prabhakaran
  *         on May 21, 2019
@@ -11,10 +14,22 @@ package com.vinu.spring;
 public class Circle implements Shape {
 	
 	private Point pointA;
+	@Autowired
+	private MessageSource messageSource;
 	
+	public MessageSource getMessageSource() {
+		return messageSource;
+	}
+
+	public void setMessageSource(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
+
 	public void draw() {
-		System.out.println("Circle drawn with center at ("+
-	    this.getPointA().getX()+","+this.getPointA().getY()+")");
+		System.out.println(this.messageSource.getMessage("circle.message", null, "Default Message to Draw Circle", null));
+		/*System.out.println("Circle drawn with center at ("+
+	    this.getPointA().getX()+","+this.getPointA().getY()+")"); */
+		System.out.println(this.messageSource.getMessage("circle.point", new Object[] {this.getPointA().getX(), this.getPointA().getY()},"Default Circle point Message", null));
 	}
 
 	public Point getPointA() {
