33c85a15c3f1a7126fb762696b4de6404c5e28df
Vinu Prabhakaran <vinu.cp@gmail.com>
2019-06-10 01:32:17 -0400

    Spring AOP - Started with a logging advice linked to generic getter method.

---
+++ SpringAOP/src/main/java/com/vinu/spring/AopMain.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.vinu.spring;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.vinu.spring.service.ShapeService;
+
+/**
+ * @author Vinu Prabhakaran
+ *         on Jun 9, 2019
+ *
+ */
+public class AopMain {
+
+
+	public static void main(String[] args) {
+		
+		ApplicationContext cntxt=new ClassPathXmlApplicationContext("spring.xml");
+	    ShapeService shapeService=cntxt.getBean("shapeService", ShapeService.class);
+	    System.out.println(shapeService.getCircle().getName());
+
+	}
+
+}
---
+++ SpringAOP/src/main/java/com/vinu/spring/model/Triangle.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.vinu.spring.model;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Vinu Prabhakaran
+ *         on Jun 10, 2019
+ *
+ */
+@Component
+public class Triangle {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
