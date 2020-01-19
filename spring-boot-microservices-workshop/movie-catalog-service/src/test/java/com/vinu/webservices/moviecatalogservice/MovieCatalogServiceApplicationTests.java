a3b1782e4b1616cdd2ff5b559b23340c8ee98a9e
Vinu Prabhakaran <vinu.cp@gmail.com>
2020-01-18 20:52:24 -0500

    spring-boot-microservices-workshop - Initialized 2 projects. Downgraded maven jar plugin to ver3.1.1 to solve issue https://bugs.eclipse.org/bugs/show_bug.cgi?id=547340

---
+++ spring-boot-microservices-workshop/movie-catalog-service/src/test/java/com/vinu/webservices/moviecatalogservice/MovieCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinu.webservices.moviecatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
