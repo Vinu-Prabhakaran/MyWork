3ece1da46ca12df62e13e9ff70cfdfc70601f429
Vinu Prabhakaran <vinu.cp@gmail.com>
2020-05-05 23:36:51 -0400

    JUnit5 - Added TestCase for add method.

---
+++ junit5-basics/src/main/java/com/vinu/junit5/basics/MathUtils.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author Vinu Prabhakaran
+ *         on May 5, 2020
+ *
+ */
+package com.vinu.junit5.basics;
+public class MathUtils {
+	
+	public int add(int a, int b) {
+		
+		return a+b;
+	}
+	
+}
---
+++ junit5-basics/src/test/java/com/vinu/junit5/basics/MathUtilsTest.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.vinu.junit5.basics;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Vinu Prabhakaran
+ *         on May 5, 2020
+ *
+ */
+ class MathUtilsTest {
+
+	/**
+	 * Test method for {@link com.vinu.junit5.basics.MathUtils#add(int, int)}.
+	 */
+	@Test
+	void testAdd() {
+
+		MathUtils mathUtil=new MathUtils();
+		assertEquals(2, mathUtil.add(1, 1), "Add Test#1 failed");
+	}
+
+}
