d46e2d2154a17d94f21c343e886a880d29ef856e
Vinu Prabhakaran <vinu.cp@gmail.com>
2020-05-08 01:41:48 -0400

    JUnit5 - Replaced duplicate instantiation of MathUtils by adding an init method with @BeforeEach annotation.

--- junit5-basics/src/test/java/com/vinu/junit5/basics/MathUtilsTest.java
+++ junit5-basics/src/test/java/com/vinu/junit5/basics/MathUtilsTest.java
@@ -5,6 +5,7 @@ package com.vinu.junit5.basics;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -13,29 +14,32 @@ import org.junit.jupiter.api.Test;
  *
  */
  class MathUtilsTest {
-
+	 
+	MathUtils mathUtil;
+	
+	@BeforeEach
+	void init(){
+		
+		mathUtil=new MathUtils();
+	}
 	/**
 	 * Test method for {@link com.vinu.junit5.basics.MathUtils#add(int, int)}.
 	 */
 	@Test
 	void testAdd() {
 
-		MathUtils mathUtil=new MathUtils();
 		assertEquals(2, mathUtil.add(1, 1), "Add Test#1 failed");
 	}
 	
 	@Test
 	void testComputeCircleArea() {
 		
-		MathUtils mathUtil=new MathUtils();
 		assertEquals(314.1592653589793, mathUtil.computeCircleArea(10));
 		
 	}
 	
 	@Test
 	void testDivide() {
-
-		MathUtils mathUtil=new MathUtils();
 
 		assertThrows(ArithmeticException.class, () -> mathUtil.divide(2, 0),"Division By Zero should return ArithmeticException");
 		assertEquals(2,mathUtil.divide(10, 5),"Division gave wrong value");
