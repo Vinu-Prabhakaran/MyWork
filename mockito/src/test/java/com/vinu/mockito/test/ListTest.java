20f9498877371b2166d2dbad3c9ecd7ebde1f0f7
Vinu Prabhakaran <vinu.cp@gmail.com>
2020-05-11 21:32:31 -0400

    Mockito - Added ListTest and mocked size method.

---
+++ mockito/src/test/java/com/vinu/mockito/test/ListTest.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.vinu.mockito.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+/**
+ * @author Vinu Prabhakaran
+ *         on May 11, 2020
+ *
+ */
+class ListTest {
+	
+	@Mock
+	List<String> mockList;
+	
+	@BeforeEach
+	public void setup() {
+	    MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	void testListSize() {
+		
+		when(mockList.size()).thenReturn(2);
+		assertEquals(2, mockList.size());
+	}
+
+}
