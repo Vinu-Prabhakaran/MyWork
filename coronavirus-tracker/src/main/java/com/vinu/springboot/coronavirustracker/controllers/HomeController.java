47cd568a51cf5ab7062079579f1d9f3bf0bc8631
Vinu Prabhakaran <vinu.cp@gmail.com>
2020-10-07 22:58:07 -0400

    coronavirus-tracker : Enabled pdf download of the report.

--- coronavirus-tracker/src/main/java/com/vinu/springboot/coronavirustracker/controllers/HomeController.java
+++ coronavirus-tracker/src/main/java/com/vinu/springboot/coronavirustracker/controllers/HomeController.java
@@ -3,10 +3,22 @@
  */
 package com.vinu.springboot.coronavirustracker.controllers;
 
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.lowagie.text.DocumentException;
+import com.vinu.springboot.coronavirustracker.models.LocationStats;
+import com.vinu.springboot.coronavirustracker.pdf.LocationStatsPdfExporter;
 import com.vinu.springboot.coronavirustracker.services.CoronaVirusDataService;
 
 /**
@@ -31,4 +43,21 @@ public class HomeController {
 				.stream().mapToInt(s -> s.getDiffFromPrevDay()).sum());
 		return "home";
 	}
+	
+	@GetMapping("/export/pdf")
+	public void exportToPDF(HttpServletResponse response) throws DocumentException, IOException {
+        response.setContentType("application/pdf");
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        String currentDateTime = dateFormatter.format(new Date());
+         
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=CoronaVirusLocationStats_" + currentDateTime + ".pdf";
+        response.setHeader(headerKey, headerValue);
+         
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+         
+        LocationStatsPdfExporter exporter = new LocationStatsPdfExporter(allStats);
+        exporter.export(response);
+         
+    }
 }
---
+++ coronavirus-tracker/src/main/java/com/vinu/springboot/coronavirustracker/pdf/LocationStatsPdfExporter.java
@@ -0,0 +1,86 @@
+/**
+ * 
+ */
+package com.vinu.springboot.coronavirustracker.pdf;
+
+import java.awt.Color;
+import java.io.IOException;
+import java.util.List;
+ 
+import javax.servlet.http.HttpServletResponse;
+ 
+import com.lowagie.text.*;
+import com.lowagie.text.pdf.*;
+import com.vinu.springboot.coronavirustracker.models.LocationStats;
+/**
+ * @author Vinu Prabhakaran
+ *         on Oct 7, 2020
+ *
+ */
+public class LocationStatsPdfExporter {
+	private List<LocationStats> listLocStats;
+    
+    public LocationStatsPdfExporter(List<LocationStats> listLocStats) {
+        this.listLocStats = listLocStats;
+    }
+ 
+    private void writeTableHeader(PdfPTable table) {
+        PdfPCell cell = new PdfPCell();
+        cell.setBackgroundColor(Color.GRAY);
+        cell.setPadding(5);
+         
+        Font font = FontFactory.getFont(FontFactory.HELVETICA);
+        font.setColor(Color.WHITE);
+         
+        cell.setPhrase(new Phrase("State", font));
+         
+        table.addCell(cell);
+         
+        cell.setPhrase(new Phrase("Country", font));
+        table.addCell(cell);
+         
+        cell.setPhrase(new Phrase("LatestTotalCases", font));
+        table.addCell(cell);
+         
+        cell.setPhrase(new Phrase("DiffFromPreviousDay", font));
+        table.addCell(cell);
+     
+    }
+     
+    private void writeTableData(PdfPTable table) {
+        for (LocationStats locStat : listLocStats) {
+            table.addCell(String.valueOf(locStat.getState()));
+            table.addCell(locStat.getCountry());
+            table.addCell(locStat.getLatestTotalCases().toString());
+            table.addCell(locStat.getDiffFromPrevDay().toString());
+        }
+    }
+     
+    public void export(HttpServletResponse response) throws DocumentException, IOException {
+        Document document = new Document(PageSize.A4);
+        PdfWriter.getInstance(document, response.getOutputStream());
+         
+        document.open();
+        Font font = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
+        font.setSize(18);
+        font.setColor(Color.ORANGE);
+         
+        Paragraph p = new Paragraph("CoronaVirus Location Statistics", font);
+        p.setAlignment(Paragraph.ALIGN_CENTER);
+         
+        document.add(p);
+         
+        PdfPTable table = new PdfPTable(4);
+        table.setWidthPercentage(100f);
+        table.setWidths(new float[] {1.5f, 3.5f, 3.0f, 3.0f,});
+        table.setSpacingBefore(10);
+         
+        writeTableHeader(table);
+        writeTableData(table);
+         
+        document.add(table);
+         
+        document.close();
+         
+    }
+}
