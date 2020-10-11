0dac06bd69bf0abafc19a768dfd6c006866c192f
Vinu Prabhakaran <vinu.cp@gmail.com>
2020-10-11 15:01:58 -0400

    Advanced JAX-RS - Added client for messanger api.

---
+++ messenger/src/main/java/com/vinu/jaxrs/messenger/client/MessengerApiClient.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.vinu.jaxrs.messenger.client;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Response;
+
+import com.vinu.jaxrs.messenger.model.Message;
+
+/**
+ * @author Vinu Prabhakaran
+ *         on Oct 11, 2020
+ *
+ */
+public class MessengerApiClient {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Client client=ClientBuilder.newClient();
+		Response response = client
+			.target("http://localhost:8080/messenger/webapi/messages/1")
+			.request()
+			.get();
+		Message message=response
+							.readEntity(Message.class);  //Unwraps the response entity.
+		System.out.println(message);
+
+	}
+
+}
