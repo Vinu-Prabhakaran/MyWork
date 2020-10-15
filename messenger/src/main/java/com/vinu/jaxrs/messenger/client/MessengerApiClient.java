437297163f8d32485c2e4beb65575488d039cdc0
Vinu Prabhakaran <vinu.cp@gmail.com>
2020-10-14 20:05:20 -0400

    Advanced JAX-RS - Modified webtarget by splitting at resource level.

--- messenger/src/main/java/com/vinu/jaxrs/messenger/client/MessengerApiClient.java
+++ messenger/src/main/java/com/vinu/jaxrs/messenger/client/MessengerApiClient.java
@@ -5,6 +5,7 @@ package com.vinu.jaxrs.messenger.client;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 
 import com.vinu.jaxrs.messenger.model.Message;
@@ -22,12 +23,20 @@ public class MessengerApiClient {
 	public static void main(String[] args) {
 		
 		Client client=ClientBuilder.newClient();
-		Response response = client
+		WebTarget baseTarget = client
+								.target("http://localhost:8080/messenger/webapi/");
+		WebTarget messageTarget = baseTarget.path("messages/");
+		WebTarget singleMessageTarget = messageTarget.path("{messageId}");
+		
+		Message message = singleMessageTarget.resolveTemplate("messageId", "1")
+					.request()
+					.get(Message.class);
+		/*Response response = client
 			.target("http://localhost:8080/messenger/webapi/messages/1")
 			.request()
 			.get();
 		Message message=response
-							.readEntity(Message.class);  //Unwraps the response entity.
+							.readEntity(Message.class);*/  //Unwraps the response entity.
 		System.out.println(message);
 
 	}
