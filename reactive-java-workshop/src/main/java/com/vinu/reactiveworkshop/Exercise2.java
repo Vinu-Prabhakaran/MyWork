d606d1c7b683891a6ed5ff6ff65fb9c4a8a596bd
Vinu Prabhakaran <vinu.cp@gmail.com>
2023-06-10 21:34:25 -0400

    Reactive Java - Exercise 2.

--- reactive-java-workshop/src/main/java/com/vinu/reactiveworkshop/Exercise2.java
+++ reactive-java-workshop/src/main/java/com/vinu/reactiveworkshop/Exercise2.java
@@ -6,13 +6,14 @@ public class Exercise2 {
 
     public static void main(String[] args) throws IOException {
 
+
         // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userFlux()
 
         // Print all numbers in the ReactiveSources.intNumbersFlux stream
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux().subscribe(System.out::println);
 
         // Print all users in the ReactiveSources.userFlux stream
-        // TODO: Write code here
+        ReactiveSources.userFlux().subscribe(System.out::println);
 
         System.out.println("Press a key to end");
         System.in.read();
