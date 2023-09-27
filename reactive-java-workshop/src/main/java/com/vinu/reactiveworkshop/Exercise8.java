6e1d8873c128116519679d8834264204afdca901
Vinu Prabhakaran <vinu.cp@gmail.com>
2023-09-26 21:22:47 -0400

    Reactive Java - Exercise 8.

--- reactive-java-workshop/src/main/java/com/vinu/reactiveworkshop/Exercise8.java
+++ reactive-java-workshop/src/main/java/com/vinu/reactiveworkshop/Exercise8.java
@@ -1,6 +1,7 @@
 package com.vinu.reactiveworkshop;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.SignalType;
 
 import java.io.IOException;
 
@@ -12,14 +13,32 @@ public class Exercise8 {
         // Use ReactiveSources.intNumbersFluxWithException()
 
         // Print values from intNumbersFluxWithException and print a message when error happens
-        // TODO: Write code here
+        ReactiveSources.intNumbersFluxWithException()
+                .subscribe(System.out::println,
+                        (e) -> System.out.println("Exception :"+e.getMessage()),
+                        () -> System.out.println("Complete"));
+        //Another solution
+        ReactiveSources.intNumbersFluxWithException()
+                .doOnError(e -> System.out.println("Exception :"+e.getMessage()))
+                .subscribe(System.out::println);
 
         // Print values from intNumbersFluxWithException and continue on errors
-        // TODO: Write code here
+        ReactiveSources.intNumbersFluxWithException()
+                .onErrorContinue( (e,n) -> System.out.println("Exception :"+e.getMessage()))
+                .subscribe(System.out::println);
 
         // Print values from intNumbersFluxWithException and when errors
         // happen, replace with a fallback sequence of -1 and -2
-        // TODO: Write code here
+        ReactiveSources.intNumbersFluxWithException()
+                .onErrorResume( e -> Flux.just(-1,-2))
+                .doFinally(signalType -> {
+                    if (signalType.equals(SignalType.ON_COMPLETE)){
+                        System.out.println("Completed Successfully");
+                    } else if (signalType.equals(SignalType.ON_ERROR)){
+                        System.out.println("Completed with error");
+                    }
+                })
+                .subscribe(System.out::println);
 
         System.out.println("Press a key to end");
         System.in.read();
