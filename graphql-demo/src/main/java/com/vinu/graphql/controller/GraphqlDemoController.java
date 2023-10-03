e222b5d1acb45aa501d877472e4139a631f7a96d
Vinu Prabhakaran <vinu.cp@gmail.com>
2023-10-03 00:01:01 -0400

    SpringGraphql - Updated to demonstrate N+1 problem.

--- graphql-demo/src/main/java/com/vinu/graphql/controller/GraphqlDemoController.java
+++ graphql-demo/src/main/java/com/vinu/graphql/controller/GraphqlDemoController.java
@@ -1,5 +1,6 @@
 package com.vinu.graphql.controller;
 
+import com.vinu.graphql.model.Department;
 import com.vinu.graphql.model.Employee;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import static com.vinu.graphql.model.Department.departmentList;
 import static com.vinu.graphql.model.Employee.employeeList;
 
 /**
@@ -38,5 +40,22 @@ public class GraphqlDemoController {
     @QueryMapping
     public Flux<Employee> employees(){
         return Flux.fromIterable(employeeList);
+    }
+
+    /**
+     * Here we demonstrate the N+1 problem.
+     * <p>If we call employees() to access all 3 employee details, there will be one call for employees()
+     * and one call each to fetch department details of each employee.
+     * <p> Effectively we end up with N+1 network calls to fulfill the graphql query</p>
+     * @param employee
+     * @return
+     */
+    @SchemaMapping(typeName = "Employee", field = "department") //field to be passed if method name doesnt match field name in schema
+    public Mono<Department> getDepartmentForEmployee(Employee employee){
+        System.out.println("Getting department details for "+employee);
+        return Mono.just(departmentList.stream()
+                .filter(d -> d.depId().equals(employee.depId()))
+                .findFirst()
+                .orElse(null));
     }
 }
--- graphql-demo/src/main/java/com/vinu/graphql/model/Employee.java
+++ graphql-demo/src/main/java/com/vinu/graphql/model/Employee.java
@@ -8,12 +8,12 @@ import static com.vinu.graphql.model.Department.departmentList;
  * @author Vinu Prabhakaran
  * on Oct 02,2023
  */
-public record Employee(String id,String name,Department department) {
+public record Employee(String id,String name, String depId) {
 
     public static final List<Employee> employeeList = List.of(
-            new Employee("emp-1","Vinu",departmentList.get(0)),
-            new Employee("emp-2","Arun",departmentList.get(0)),
-            new Employee("emp-3","Arjun",departmentList.get(1))
+            new Employee("emp-1","Vinu","dep-1"),
+            new Employee("emp-2","Arun","dep-2"),
+            new Employee("emp-3","Arjun","dep-1")
             );
 
 }
